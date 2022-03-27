8bb9171375aebba89eedce007d660d644515ffc0
Raph <93734816+RxphGui@users.noreply.github.com>
2022-03-27 23:05:06 +0200

    Add files via upload

---
+++ best tableaux/main.php
@@ -0,0 +1,15 @@
+<?php
+$raph = [
+    [   
+        'CTF' => 'No Team',
+        'Cybersec' => 'Reverse & Pwn',
+        'Age' => '17 ans',
+        'Love' => 'Maths'
+    ]
+];
+
+echo $raph[0]['CTF'];
+echo $raph[0]['Cybersec'];
+echo $raph[0]['Age'];
+echo $raph[0]['Love'];
+?>
